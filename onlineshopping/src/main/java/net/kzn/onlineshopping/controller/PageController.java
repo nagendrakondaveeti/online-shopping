6e4176b920177d51623ea8c83c4945e350b5feb1
Nagendra Kondaveeti <nagpuligadda@gmail.com>
2018-08-10 01:50:19 +0530

    Completed the interface theme creation successfully.

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -10,9 +10,25 @@ public class PageController {
 	@RequestMapping(value= {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mav=new ModelAndView("page");
-		mav.addObject("greeting","Welcome to spring mvc");
+		mav.addObject("title","HomePage");
+		mav.addObject("userClickHome",true);
 		return mav;
 	}
 	
+	@RequestMapping(value="/about")
+	public ModelAndView about() {
+		ModelAndView mav=new ModelAndView("page");
+		mav.addObject("title","About us");
+		mav.addObject("userClickAbout",true);
+		return mav;
+	}
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contact() {
+		ModelAndView mav=new ModelAndView("page");
+		mav.addObject("title","Contact us");
+		mav.addObject("userClickContact",true);
+		return mav;
+	}
 
 }
